dfc7f6166ed85e4169cb28df4ccc659767214cc1
HARSH MEHTA <53077349+HarshMehta205@users.noreply.github.com>
2019-11-18 17:16:58 +0530

    Create week4_4.java

---
+++ week4_4.java
@@ -0,0 +1,46 @@
+//The program in this assignment is attempted to print the following output: 
+//-----------------OUTPUT-------------------
+//This is large
+//This is medium
+//This is small
+//This is extra-large
+
+//-------------------------------------------------
+
+interface ExtraLarge{
+	String extra = "This is extra-large";
+	public void display();
+}
+
+class Large {
+    public void Print() {
+        System.out.println("This is large");
+    }
+}
+ 
+class Medium extends Large {
+    public void Print() {
+    	  super.Print();  
+        System.out.println("This is medium");
+    }
+}
+class Small extends Medium {
+    public void Print() {
+        super.Print();  
+        System.out.println("This is small");
+    }
+}
+ 
+class Question43 implements ExtraLarge{
+
+	public void display(){
+		System.out.print(extra);
+	}
+
+    public static void main(String[] args) {
+        Small s = new Small();
+        s.Print();
+	Question43 q = new Question43();
+	  q.display();
+    }
+}
